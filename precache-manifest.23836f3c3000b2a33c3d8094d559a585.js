52b82d97041728f9c5767fe5df55af7cc33f758d
github username <justinzhao265@gmail.com>
2020-10-07 05:30:11 -0400

    Updates

---
+++ precache-manifest.23836f3c3000b2a33c3d8094d559a585.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d29caf6959dd02a3327f8490288ca54",
+    "url": "/FantasyRedraw/index.html"
+  },
+  {
+    "revision": "7f0e0f48de30afb1eb34",
+    "url": "/FantasyRedraw/static/css/main.e5fd1a4c.chunk.css"
+  },
+  {
+    "revision": "39fadc65a7f7cd8fa1af",
+    "url": "/FantasyRedraw/static/js/2.0a0af733.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/FantasyRedraw/static/js/2.0a0af733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0e0f48de30afb1eb34",
+    "url": "/FantasyRedraw/static/js/main.c103ecb4.chunk.js"
+  },
+  {
+    "revision": "2ccbc2a599c54c2b2340",
+    "url": "/FantasyRedraw/static/js/runtime-main.203bf083.js"
+  },
+  {
+    "revision": "9bad488e9e44d8c96e7da41a4051cf83",
+    "url": "/FantasyRedraw/static/media/bg.9bad488e.png"
+  },
+  {
+    "revision": "d04672558fca27448c46cd9179206895",
+    "url": "/FantasyRedraw/static/media/title.d0467255.png"
+  }
+]);
